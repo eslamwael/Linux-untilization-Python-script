2e7d514e585995a5614baa0cedcd90639d37c500
Eslam Wael <35847720+eslamwael@users.noreply.github.com>
2020-12-09 12:32:50 +0200

    Update util.py

--- util.py
+++ util.py
@@ -6,10 +6,10 @@ import datetime as dt # Retreive current time
 ## == CPU Utilization == ##
 # psutil.cpu_percent: return CPU utilization as percentage
 cpu_csvRow = [dt.datetime.now(),psutil.cpu_percent(interval=1, percpu=False)]
-cpu_csv = "/opt/test_cpu.csv"
+cpu_csv = "/opt/CPU.csv"
 
 # Appending results in the csv file
-with open(cpu_csv, "a", newline='') as fp:
+with open(cpu_csv, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(cpu_csvRow)
 
@@ -19,10 +19,10 @@ with open(cpu_csv, "a", newline='') as fp:
 mem_free_per = round((100 - psutil.virtual_memory().percent),2)
 
 mem_csvRow = [dt.datetime.now(),mem_free_per]
-mem_csv = "/opt/test_mem.csv"
+mem_csv = "/opt/MEM.csv"
 
 # Appending results in the csv file
-with open(mem_csv, "a", newline='') as fp:
+with open(mem_csv, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(mem_csvRow)
 
@@ -32,9 +32,9 @@ with open(mem_csv, "a", newline='') as fp:
 disk_free_per = round((100 - psutil.disk_usage('/').percent),2)
 
 disk_csvRow = [dt.datetime.now(),disk_free_per]
-disk_csv = "/opt/test_disk.csv"
+disk_csv = "/opt/DISK.csv"
 
 # Appending results in the csv file
-with open(disk_csv, "a", newline='') as fp:
+with open(disk_csv, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(disk_csvRow)
